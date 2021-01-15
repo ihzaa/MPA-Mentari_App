3db590439f2809011a3bd9b6e4b7aa46b0739ae9
yusufrnp <yusufrnp@gmail.com>
2021-01-15 13:50:50 +0700

    database

---
+++ app/Models/address.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class address extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function transactions()
+    {
+        return $this->hasMany(transaction::class);
+    }
+    public function customer()
+    {
+        return $this->belongsTo(customer::class);
+    }
+}
---
+++ database/migrations/2021_01_15_055402_create_favorites_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('item_id');
+            $table->unsignedBigInteger('customer_id');
+            $table->timestamps();
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
