2e585b386d79036f9a1ba92329167b2e84bd7fdd
yusufrnp <yusufrnp@gmail.com>
2021-01-20 13:48:40 +0700

    template backend

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('FrontEnd.templates.all');
 });
+Route::get('/admin', function () {
+    return view('BackEnd.templates.all');
+});
