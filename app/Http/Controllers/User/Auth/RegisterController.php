4570d05253934395290a15dcc58149a4adc0c147
ihzaahmad0 <ihzaahmad0@gmail.com>
2021-06-08 14:34:06 +0700

    user register controller

---
+++ app/Http/Controllers/User/Auth/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\User\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|unique:email|email',
+            'password' => 'required|min:8',
+            'nama' => 'required',
+            'phone' => 'numeric'
+        ]);
+
+        $user = User::create([
+            'password' => Hash::make($request->password),
+            'name' => $request->nama,
+            'email' => $request->email,
+            'phone' => $request->phone
+        ]);
+
+        Auth::guard('user')->loginUsingId($user->id);
+        return 'register berhasil dan otomatis login dan redirect ke halaman home';
+
+    }
+}
