b943e126ac2472b41625996684100b9c741ade4a
ihzaahmad0 <ihzaahmad0@gmail.com>
2021-06-08 13:42:50 +0700

    controller login user

---
+++ app/Http/Controllers/User/Auth/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\User\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function loginGet()
+    {
+        return 'ini kasih return ke blade halaman login';
+    }
+
+    public function loginPost(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        $user = User::whereEmail($request->email)->first();
+
+        if ($user == []) {
+            return back()->with('icon', 'error')->with('title', 'Maaf')->with('text', 'email atau password salah!');
+        }
+        if(Hash::check($request->password, $user->password)){
+            $remember = $request->has('remember') ? true : false;
+            Auth::guard('user')->loginUsingId($user->id, $remember);
+            // return redirect()->intended(route('namenya route home'));
+            return 'login berhasil';
+        }
+        return back()->with('icon', 'error')->with('title', 'Maaf')->with('text', 'email atau password salah!');
+    }
+
+    public function logout()
+    {
+        Auth::guard('user')->logout();
+        return 'Logout berhasil dan redirect ke halaman home';
+    }
+}
