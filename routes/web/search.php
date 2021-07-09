a157c8bf2b9565b6a0d37a67c0870a8530401320
yusufrnp <yusufrnp@gmail.com>
2021-07-09 12:56:43 +0700

    home fix

--- routes/web/search.php
+++ routes/web/search.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\User\HomeController;
-use Illuminate\Support\Facades\Route;
-
-Route::name('user.')->group(function () {
-    Route::get('', [HomeController::class, 'home'])->name('search');
-});
+// Route::name('user.')->group(function () {
+//     Route::get('', [HomeController::class, 'index'])->name('search');
+// });
